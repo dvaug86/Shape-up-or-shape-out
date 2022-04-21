0d315fee020e90e86e99ea142f8ed33b547e6e07
David Vaughan <dvaug86@gmail.com>
2022-04-21 09:03:51 -0400

    index page setup and stylized

---
+++ apps.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", function () {
+// wrtext
+// var rect_width = parseInt(document.getElementById("wrtext").value);
+// rect_width.submit(function(e){
+//     e.preventDefault();
+
+// console.log(wrtext);
+// });
+// });
